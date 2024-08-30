13428f7436e7c8b2f5d50dd2338ee28bb912489a
ZhangShaotian <winchester.z.work@gmail.com>
2024-08-30 09:29:45 -0400

    chore(sdb): remove unnecessary commented code
    Remove the commented-out line `//#include "test-expr.h"` from `sdb.c`. This cleanup improves code readability by eliminating unused code.

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,7 +18,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
-//#include "test-expr.h"// Just a try, should remove it later
 
 static int is_batch_mode = false;
 
